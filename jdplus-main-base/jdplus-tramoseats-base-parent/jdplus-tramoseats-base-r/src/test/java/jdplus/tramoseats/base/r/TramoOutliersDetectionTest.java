c5679d9b9700ebdbdfefb4c1265ce0d0a47216ea
Palate Jean <jean.palate@nbb.be>
2023-05-24 09:59:03 +0200

    Burman estimates, Outliers detection with missing values

--- jdplus-main-base/jdplus-tramoseats-base-parent/jdplus-tramoseats-base-r/src/test/java/jdplus/tramoseats/base/r/TramoOutliersDetectionTest.java
+++ jdplus-main-base/jdplus-tramoseats-base-parent/jdplus-tramoseats-base-r/src/test/java/jdplus/tramoseats/base/r/TramoOutliersDetectionTest.java
@@ -5,6 +5,7 @@
  */
 package jdplus.tramoseats.base.r;
 
+import jdplus.toolkit.base.api.timeseries.TsData;
 import tck.demetra.data.Data;
 import org.junit.jupiter.api.Test;
 
@@ -22,7 +23,7 @@ public class TramoOutliersDetectionTest {
     @Test
     public void testProd() {
 
-        TramoOutliersDetection.Results rslts = TramoOutliersDetection.process(Data.TS_PROD, new int[]{0, 1, 1}, new int[]{0, 1, 1}, false, null, true, true, false, true, 4, false);
+        TramoOutliersDetection.Results rslts = TramoOutliersDetection.process(Data.TS_PROD, new int[]{0, 1, 1}, new int[]{0, 1, 1}, false, null, true, true, false, true, 4, false, false);
         String[] outliers = rslts.getData(TramoOutliersDetection.Results.BNAMES, String[].class);
 //        if (outliers != null) {
 //            for (int i = 0; i < outliers.length; ++i) {
@@ -32,4 +33,15 @@ public class TramoOutliersDetectionTest {
         assertTrue(outliers == null);
     }
 
+    @Test
+    public void testProdMissing() {
+        double[] x=Data.PROD.clone();
+        x[10]=Double.NaN;
+        x[100]=Double.NaN;
+        x[200]=10;
+        TsData X=TsData.ofInternal(Data.TS_PROD.getStart(), x);
+        TramoOutliersDetection.Results rslts = TramoOutliersDetection.process(X, new int[]{0, 1, 1}, new int[]{0, 1, 1}, false, null, true, true, false, true, 4, false, false);
+        String[] outliers = rslts.getData(TramoOutliersDetection.Results.BNAMES, String[].class);
+        assertTrue(outliers.length == 1);
+    }
 }
